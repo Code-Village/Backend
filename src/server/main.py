8d983f3cfdcd432084440a5a601b07e30ff099d6
YongjaeHyun <dltjrrbs2020@gmail.com>
2022-08-10 20:20:51 +0900

    [API 1] API문서 생성

--- src/server/main.py
+++ src/server/main.py
@@ -13,4 +13,6 @@ api.add_namespace(Hello, '/hello')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "5000"))
+    if port == 5000:
+        app.run(host="0.0.0.0", port=port, debug=True)
     app.run(host="0.0.0.0", port=port)
