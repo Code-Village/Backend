cf3d317e21b635cb3d641e007aeb6aef0825fcee
YongjaeHyun <dltjrrbs2020@gmail.com>
2022-08-11 12:34:20 +0900

    [DEV 2] Hello API post, get 메서드 구현

--- src/server/api/hello.py
+++ src/server/api/hello.py
@@ -1,6 +1,8 @@
 from flask import request
 from flask_restx import Resource, Api, Namespace, fields
 
+DATABASE = {"data": "아무튼 빅-데이터"}
+
 Hello = Namespace(
     name="Hello",
     description="테스트를 위해 작성한 API.",
@@ -20,4 +22,21 @@ class HelloPost(Resource):
         return {
             '님이 적은 것': data
         }, 201
+
+@Hello.route('/<string:data>')
+@Hello.doc(params={'data': 'key값'})
+class HelloGet(Resource):
+    @Hello.response(200, "Success")
+    @Hello.response(500, 'Failed')
+    def get(self, data):
+        """데이터를 출력함."""
+        res = DATABASE.get(data)
+        if res:
+            return {
+                'data': res
+            }, 200
+        else:
+            return{
+                'data': "그런 거 없음..."
+            }, 500
         
